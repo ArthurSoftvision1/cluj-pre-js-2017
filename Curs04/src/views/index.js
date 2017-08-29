7a3d9832e1e2a11c7254229458e52baf9cd799c4
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-08-29 10:34:38 +0300

    Commented code

--- Curs04/src/views/index.js
+++ Curs04/src/views/index.js
@@ -1,17 +1,23 @@
 
 window.onload = function(){
     const app = document.querySelector('#app');
+    // call LoginPage function and display LoginPage using innerHTML method
     app.innerHTML = LoginPage();
 
+    // get the form by his ID
    const loginFormSection = document.getElementById("form-login")
 
     const myObj = {}
 
    const goToNewEvaluationPage = function(event) {
         event.preventDefault();
+        // call NewEvaluationPage() which will render the New-evaluation page
         app.innerHTML = NewEvaluationPage()
 
+        // get evaluationButton by his ID
         const evaluationButton = document.getElementById("evaluation-page")
+
+        // add "click" event listener
         evaluationButton.addEventListener("click", goToEvaluationsPage)
 
         const getSelectorAll = document.getElementsByTagName("select")
@@ -25,37 +31,64 @@ window.onload = function(){
         })
     }
 
+    // go to evaluation page function
     const goToEvaluationsPage = function() {
+        // call EvaluationPage function and display EvaluationPage using innerHTML method
         app.innerHTML = EvaluationsPage();
         const newEvaluationButton = document.getElementById("new-evaluation-page")
 
+        // add click event listener on newEvaluationButton
         newEvaluationButton.addEventListener("click", goToNewEvaluationPage)
     }
 
+    // Create submitButton function
+    const submitButton = function(event) {
+        event.preventDefault();
+        
+        // get button by his ID #submit-button
+        const submitFormButton = document.getElementById("submit-button")
+    }
+
+    // addEventListener on submit
+    submitFormButton.addEventListener("submit", submitButton)
+
+
+    // submitLogin function
     const submitLogin = function(event) {
         event.preventDefault();
 
+        // get username input value by his ID
         const usernameValue = document.getElementById("username-field").value
+
+        // get password input value by his ID
         const passwordValue = document.getElementById("password-field").value
 
+        // set alert message if the inputs not meet the conditions
         const displayAlert = function(message){
             const verify = document.getElementById('alert') || false;
             if(!verify){
-                const alertDiv = document.createElement('div'); // create alert div
+                // create alert div where the message will be displayed
+                const alertDiv = document.createElement('div'); 
+                // call parameter function = message
                 alertDiv.innerHTML = message;
+                // set alertDiv ID
                 alertDiv.id = "alert";
                 loginFormSection.appendChild(alertDiv);
             }
         }
 
+        // check of username input and password input contain value 1
         if (usernameValue === "1" && passwordValue === "1") {
+            // call goToEvaluationPage function which will render the page
             goToEvaluationsPage()
         } else {
+            // if the condition is not met, drop this message
             displayAlert("Something went wrong");
         }
 
     }
 
+    // add "submit" event listener on login button
    loginFormSection.addEventListener("submit", submitLogin)
 
 
