b882160ac1d5afcb0cf92534e6aadd6f755f3e5f
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-08-31 16:45:41 +0300

    CommonEvents

--- Curs04/src/events/commonEvents.js
+++ Curs04/src/events/commonEvents.js
@@ -2,11 +2,10 @@ const commonEvents = function() {
 
     const goToTheLoginPage = function() {
         loadPage('login') // show loginPage
-        sessionStorage.removeItem("LoggedIn")
+        sessionStorage.removeItem("LoggedIn") // remove the item from sessionStorage
     }
 
-    const logoutButton = document.getElementById("logout-button")
+    const logoutButton = document.getElementById("logout-button") // get logoutButton by his ID
 
-    // add click event listener on lgoutButton
-    logoutButton.addEventListener("click", goToTheLoginPage)
+    logoutButton.addEventListener("click", goToTheLoginPage) // add click event listener on lgoutButton
 }
