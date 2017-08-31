16d13334e99a41584f65836fd8b6e4ebf451abe9
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-08-31 16:46:33 +0300

    Evaluation Events

--- Curs04/src/events/evaluationEvents.js
+++ Curs04/src/events/evaluationEvents.js
@@ -1,12 +1,17 @@
 const evaluationEvents = function() {
 
     const goToTheNewEvaluationPage = function() {
-        loadPage('new-evaluation') // show loginPage
+        loadPage('new-evaluation') // show new-evaluation page
     }
    
-    const newEvaluationButton = document.getElementById("new-evaluation-page")
+    const newEvaluationButton = document.getElementById("new-evaluation-page") // get newEvaluationButton by his ID
 
-    // add click event listener on newEvaluationButton
-    newEvaluationButton.addEventListener("click", goToTheNewEvaluationPage)
+    newEvaluationButton.addEventListener("click", goToTheNewEvaluationPage) // add click event listener on newEvaluationButton
+
+    const addDetailsButton = document.getElementById("add-button")
+
+    addDetailsButton.addEventListener("click", goToTheNewEvaluationPage)
+
+    
     
 }
