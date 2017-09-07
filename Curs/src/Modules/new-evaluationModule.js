507d550ee914e9e568cfcb9c4f127068fdf1b139
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2017-09-07 15:24:45 +0300

    NewEvaluation Promise

--- Curs/src/Modules/new-evaluationModule.js
+++ Curs/src/Modules/new-evaluationModule.js
@@ -1,19 +1,38 @@
 interviewApp.newEvaluationModule = {
 
     init() {
+        const newPromise = new Promise(function (resolve, reject) {
+            const xhttp = new XMLHttpRequest();
 
-        const xhttp = new XMLHttpRequest();
-        xhttp.open('GET', 'src/Data/xhrNew.json', true)
-        xhttp.send()
-
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-            
-                const newObject = JSON.parse(xhttp.responseText);
-                app.innerHTML = interviewApp.NewEvaluationPage(newObject);
-                interviewApp.newEvaluationEvents(); // call newEvaluation events function
-                interviewApp.commonEvents(); // call commonEvents function which will load the login page
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    if (this.status === 200) {
+                        var newEvalObject;
+                        try {
+                            newEvalObject = JSON.parse(xhttp.responseText);
+                        } catch (e) {
+                            reject('Something went wrong')
+                        }
+                        resolve(newEvalObject);
+                    } else {
+                        reject('Status wrong')
+                    }
+                }
             }
-        };      
+
+            xhttp.open('GET', 'src/Data/xhrNew.json', true)
+            xhttp.send()
+        });
+        newPromise.then((newObject) => {
+            app.innerHTML = interviewApp.NewEvaluationPage(newObject);
+            interviewApp.newEvaluationEvents(); // call newEvaluation events function
+            interviewApp.commonEvents(); // call commonEvents function which will load the login page
+        }).catch((error) => {
+            alert(error);
+        });
+
     }
 }
+
+
+
